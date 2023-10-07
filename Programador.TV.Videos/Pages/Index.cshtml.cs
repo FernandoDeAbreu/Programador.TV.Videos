b0eca5bad70d2d824d7b62fb3b4d11b16eb0b7a2
Fernando Abreu <fabreu@narwalsistemas.com.br>
2023-10-07 11:56:14 -0300

    Funcionalidades Iniciais * Pagina inicial para upar e exibir os dados video * JS para não recarregar a pagina * Back para coletar e devolver os dados do video em JSON

--- Programador.TV.Videos/Pages/Index.cshtml.cs
+++ Programador.TV.Videos/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Programador.TV.Videos.Pages
 {
@@ -14,7 +15,28 @@ namespace Programador.TV.Videos.Pages
 
         public void OnGet()
         {
+        }
 
+        public async Task<IActionResult> OnPostAsync(IFormFile videoFile)
+        {
+            if (videoFile == null || videoFile.Length <= 0)
+            {
+                return BadRequest("Nenhum arquivo de vídeo enviado.");
+            }
+
+            var videoInfo = new
+            {
+                Nome = videoFile.FileName,
+                TamanhoEmBytes = videoFile.Length,
+                TipoDeMídia = videoFile.ContentType,
+                Extensao = Path.GetExtension(videoFile.FileName),
+                DataDeUpload = DateTime.Now,
+            };
+
+           
+            var json = JsonConvert.SerializeObject(videoInfo, Formatting.Indented);
+
+            return Content(json, "application/json");
         }
     }
 }
---
+++ Programador.TV.Videos/wwwroot/js/upload.js
@@ -0,0 +1,14 @@
+﻿document.getElementById('formEntrada').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var formData = new FormData(event.target);
+
+    fetch('/Index', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('videoInfo').innerText = JSON.stringify(data);
+        });
+});
